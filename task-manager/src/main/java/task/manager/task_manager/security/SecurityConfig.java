2c7613198066154e32b18d5a678a0a06f139985a
samuel-amo <samuelamo55@outlook.com>
2025-01-27 01:21:07 +0000

    feat: configure JWT authentication and security filter chain

---
+++ task-manager/src/main/java/task/manager/task_manager/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package task.manager.task_manager.security;
+
+import com.example.task_management.jwt.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AppUserDetailService appUserDetailService;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, AppUserDetailService appUserDetailService) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.appUserDetailService = appUserDetailService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests( authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/v1/auth/**")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+}
