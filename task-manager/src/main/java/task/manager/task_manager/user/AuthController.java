3724d9da7cedd6b8507fd60c37100adc3a2b8493
samuel-amo <samuelamo55@outlook.com>
2025-01-27 01:15:54 +0000

    feat: add authentication controller
    - Introduced AuthController to handle user registration and login endpoints.
    - Exposed `/api/v1/auth/register` and `/api/v1/auth/login` routes for user operations.

---
+++ task-manager/src/main/java/task/manager/task_manager/user/AuthController.java
@@ -0,0 +1,33 @@
+package task.manager.task_manager.user;
+
+
+import com.example.task_management.task.ApiSuccessResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+    public AuthController(AuthServiceImpl authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiSuccessResponse> register(@Valid @RequestBody SignUpRequest signUpRequest) {
+        authService.register(signUpRequest);
+        return ResponseEntity.ok(new ApiSuccessResponse("Successfully registered"));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        authService.login(loginRequest);
+        return ResponseEntity.ok(authService.login(loginRequest));
+    }
+
+}
