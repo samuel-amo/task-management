f5c37f3bcd08e253e590bce36bdaed8809e426e1
samuel-amo <samuelamo55@outlook.com>
2025-01-27 01:20:47 +0000

    feat: implement AppUserDetails and AppUserDetailService for user authentication

---
+++ task-manager/src/main/java/task/manager/task_manager/security/AppUserDetailService.java
@@ -0,0 +1,25 @@
+package task.manager.task_manager.security;
+
+import com.example.task_management.user.AppUser;
+import com.example.task_management.user.AppUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserDetailService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+
+    public AppUserDetailService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
+        return new AppUserDetails(appUser);
+    }
+}
---
+++ task-manager/src/main/java/task/manager/task_manager/security/AppUserDetails.java
@@ -0,0 +1,27 @@
+package task.manager.task_manager.security;
+
+import com.example.task_management.user.AppUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+
+public record AppUserDetails(AppUser user) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
