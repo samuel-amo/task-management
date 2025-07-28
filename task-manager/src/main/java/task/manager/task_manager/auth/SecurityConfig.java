916050b0814e7cf63f746e3294916f2cf8f0313e
SamuelAmo <samuelamo55@outlook.com>
2025-07-28 11:35:32 +0000

    feat: allow public access to actuator endpoints in security configuration

--- task-manager/src/main/java/task/manager/task_manager/auth/SecurityConfig.java
+++ task-manager/src/main/java/task/manager/task_manager/auth/SecurityConfig.java
@@ -33,8 +33,8 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests( authorizeRequests -> authorizeRequests
-                        .requestMatchers("/api/v1/auth/**")
-                        .permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception -> exception
